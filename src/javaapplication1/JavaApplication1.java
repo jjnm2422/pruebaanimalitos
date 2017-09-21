224f5f519b1f98480f639432b9f792aa5f521f8b
Administrador <Administrador@Navarro-PC>
2017-09-20 23:32:29 -0400

    Inicio de proyecto

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,209 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Administrador
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int acum=1000;
+        int ganancia =0;
+        int gananciae =0;
+        int monto = 1000;
+        int max = 0;
+        int i=0;
+            ganancia = monto*30;
+            gananciae = ganancia-acum;
+            System.out.println("Jugada: "+(i)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        for (i = 0; i <= 11; i++) {
+            monto += 200;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+            System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        }
+        System.out.println("\n");
+        
+        monto += 300;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        System.out.println("\n");
+        
+        monto += 300;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        System.out.println("\n");
+        
+        for (i=0; i <= 6; i++) {
+          monto += 200;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       }
+        System.out.println("\n");
+        
+         monto += 300;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        System.out.println("\n");
+        
+        
+        monto += 300;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       System.out.println("\n");
+       
+        for (i=0; i <= 2; i++) {
+           monto += 200;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       }
+        System.out.println("\n");
+        
+       monto += 400;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        System.out.println("\n");
+        
+        for (i=0; i <= 6; i++) {
+           monto += 200;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       }
+        monto += 600;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        System.out.println("\n");
+        for (i=0; i <= 10; i++) {
+           monto += 200;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       }
+        System.out.println("\n");
+        //igual
+        ganancia = monto*30;
+        acum += monto;
+        gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       //fin
+       System.out.println("\n");
+        monto += 200;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+         System.out.println("\n");
+           monto += 600;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       //12000
+       System.out.println("\n");
+        ganancia = monto*30;
+        acum += monto;
+        gananciae = ganancia-acum;
+        System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       System.out.println("\n");
+        for (i=0; i <= 1; i++) {
+           monto += 200;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       }
+        System.out.println("\n");
+        monto += 600;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       //aumento de 100
+        System.out.println("\n***********************jugadas mañana*************************");
+       for (i=0; i <= 20; i++) {
+           monto += 500;
+            ganancia = monto*30;
+            acum += monto;
+            gananciae = ganancia-acum;
+           System.out.println("Jugada: "+(i+1)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+       }
+        
+       
+    /*   System.out.println("\n***********************jugadas jjnm*************************");
+        acum=0;
+        ganancia =0;
+        gananciae =0;
+        monto = 0;
+        i=0;
+        int j=0;
+        int k=10;
+//            ganancia = monto*30;
+//            gananciae = ganancia-acum;
+//            System.out.println("Jugada: "+(i)+" monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+        for (i = 0; i <= 79; i++) {
+            if (i%8==0) {
+               j++;
+                System.out.println("\nDia "+j+"\n");
+            }
+            monto += 50;
+            acum += monto;
+            ganancia = monto*30;
+            gananciae = ganancia-acum;
+            if (gananciae==0) {
+                System.out.println("###################Aqui no ganas ni pierdes###########################");
+            }
+            System.out.println((i+1)+". Sorteo de las "+(k)+": monto jugado: "+monto+" Monto Acumulado: "+acum+" Ganancia Neta: "+ganancia+" Ganancia Estimada: "+gananciae);
+            k++;
+            if (gananciae>max) {
+                max = gananciae;
+            }
+            if(k==13){
+                k=1;
+            }else if (k==2){
+                k=4;
+            }else if (k==8){
+                k=10;
+            }
+        }
+        System.out.println("\nGanancia Maxima: "+max);*/
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
